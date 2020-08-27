65b85347e3e8bfe8b59931112e8f99afc4b02e9a
Domic <dominicbertocchini@gmail.com>
2020-08-27 08:57:06 -0700

    basic movement and test terrain

---
+++ In-Your-Shoes/Assets/Scripts/PlayerMovementScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementScript : MonoBehaviour
+{
+    public float accelSpeed = 1.0F;
+    public float speed = 6.0F;
+    public float jumpSpeed = 8.0F;
+    public float gravity = 10.0F;
+    private Vector2 velocity = Vector2.zero;
+    CharacterController controller;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetKey("a"))
+            velocity.x -= accelSpeed * Time.deltaTime;
+        if (Input.GetKey("d"))
+            velocity.x += accelSpeed * Time.deltaTime;
+        if (controller.isGrounded && Input.GetButton("Jump"))
+            velocity.y = jumpSpeed;
+             
+        if (velocity.x > speed)
+            velocity.x = speed;
+        else if (velocity.x < -speed)
+            velocity.x = -speed;
+        velocity.y -= gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+     }
+}

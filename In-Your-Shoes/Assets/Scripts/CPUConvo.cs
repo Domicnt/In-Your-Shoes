d3bd42e6acaf2b44fdb6a3b94e1951d83e739e3c
jchou21 <jaredhappy@gmail.com>
2020-09-02 12:20:02 -0700

    working on adding dialogue with other climber

---
+++ In-Your-Shoes/Assets/Scripts/CPUConvo.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CPUConvo : MonoBehaviour
+{
+    public GameObject player;
+    public DialogueTrigger dialogueTrigger;
+    private int hasTalked;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        hasTalked = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distFromPlayer = Vector2.Distance(player.transform.position, this.transform.position);
+        if (distFromPlayer <= 100 && hasTalked == 0)
+        {
+            dialogueTrigger.TriggerDialogue();
+        }
+    }
+}
